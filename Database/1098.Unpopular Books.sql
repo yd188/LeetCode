3054243298b4669599538f98b9667e8da2a1a3cf
ydong188 <39896319+ydong188@users.noreply.github.com>
2019-11-18 11:52:28 -0800

    update

--- Database/1098.Unpopular Books.sql
+++ Database/1098.Unpopular Books.sql
@@ -67,9 +67,9 @@ WITH total_ordered AS
 		,SUM(NVL(b.quantity,0)) AS ordered
 FROM books AS a
 LEFT JOIN orders AS b
-ON a.book_id=b.book_id AND a.available_from<DATEADD(day,-30,sysdate)
-GROUP BY a.name
-)
+--ON a.book_id=b.book_id AND a.available_from<DATEADD(day,-30,sysdate)
+ON a.book_id=b.book_id AND DATEDIFF(day, a.available_from , sysdate)>30
+GROUP BY a.name)
 SELECT  book_id
 		,name
 FROM total_ordered
