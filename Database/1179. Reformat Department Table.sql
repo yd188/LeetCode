0e2bccddf99713ed16ec949cef992c0a5a763235
yd188 <39896319+yd188@users.noreply.github.com>
2019-11-24 11:48:49 -0800

    Update 1179. Reformat Department Table.sql

--- Database/1179. Reformat Department Table.sql
+++ Database/1179. Reformat Department Table.sql
@@ -40,18 +40,18 @@ Result table:
 Note that the result table has 13 columns (1 for the department id + 12 for the months).
 */
 
-select id,
-        sum(case when month = 'Jan' then revenue end) as Jan_Revenue,
-        sum(case when month = 'Feb' then revenue end) as Feb_Revenue,
-        sum(case when month = 'Mar' then revenue end) as Mar_Revenue,
-        sum(case when month = 'Apr' then revenue end) as Apr_Revenue,
-        sum(case when month = 'May' then revenue end) as May_Revenue,
-        sum(case when month = 'Jun' then revenue end) as Jun_Revenue,
-        sum(case when month = 'Jul' then revenue end) as Jul_Revenue,
-        sum(case when month = 'Aug' then revenue end) as Aug_Revenue,
-        sum(case when month = 'Sep' then revenue end) as Sep_Revenue,
-        sum(case when month = 'Oct' then revenue end) as Oct_Revenue,
-        sum(case when month = 'Nov' then revenue end) as Nov_Revenue,
-        sum(case when month = 'Dec' then revenue end) as Dec_Revenue
-from department
-group by 1;
+SELECT id,
+        SUM(CASE WHEN month = 'Jan' THEN revenue END) AS Jan_Revenue,
+        SUM(CASE WHEN month = 'Feb' THEN revenue END) AS Feb_Revenue,
+        SUM(CASE WHEN month = 'Mar' THEN revenue END) AS Mar_Revenue,
+        SUM(CASE WHEN month = 'Apr' THEN revenue END) AS Apr_Revenue,
+        SUM(CASE WHEN month = 'May' THEN revenue END) AS May_Revenue,
+        SUM(CASE WHEN month = 'Jun' THEN revenue END) AS Jun_Revenue,
+        SUM(CASE WHEN month = 'Jul' THEN revenue END) AS Jul_Revenue,
+        SUM(CASE WHEN month = 'Aug' THEN revenue END) AS Aug_Revenue,
+        SUM(CASE WHEN month = 'Sep' THEN revenue END) AS Sep_Revenue,
+        SUM(CASE WHEN month = 'Oct' THEN revenue END) AS Oct_Revenue,
+        SUM(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Revenue,
+        SUM(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
+FROM department
+GROUP BY 1;
