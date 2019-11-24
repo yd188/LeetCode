72f7a2ff4ac1909c2c2afb568c751ac7102df745
yd188 <39896319+yd188@users.noreply.github.com>
2019-11-24 11:29:36 -0800

    update format

--- Database/1098. Unpopular Books.sql
+++ Database/1098. Unpopular Books.sql
@@ -63,8 +63,8 @@ Result table:
 
 WITH total_ordered AS
 (SELECT  a.book_id
-		,a.name
-		,SUM(NVL(b.quantity,0)) AS ordered
+	,a.name
+	,SUM(NVL(b.quantity,0)) AS ordered
 FROM books AS a
 LEFT JOIN orders AS b
 --ON a.book_id=b.book_id AND a.available_from<DATEADD(day,-30,sysdate)
