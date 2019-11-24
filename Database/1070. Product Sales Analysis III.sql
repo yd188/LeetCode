57c3547cd2c6cc3ed6f6256e172df00e52e17512
ydong188 <39896319+ydong188@users.noreply.github.com>
2019-11-23 20:24:31 -0800

    add 1070

---
+++ Database/1070. Product Sales Analysis III.sql
@@ -0,0 +1,77 @@
+/*
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+sale_id is the primary key of this table.
+product_id is a foreign key to Product table.
+Note that the price is per unit.
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key of this table.
+ 
+
+Write an SQL query that selects the product id, year, quantity, and price for the first year of every product sold.
+
+The query result format is in the following example:
+
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+
+Result table:
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
+*/
+--Solution 1:
+SELECT DISTINCT product_id
+		,FIRST_VALUE(year) OVER (PARTITION BY product_id ORDER BY first_year ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS first_year
+		,FIRST_VALUE(quantity) OVER (PARTITION BY product_id ORDER BY first_year ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS quantity
+		,FIRST_VALUE(price) OVER (PARTITION BY product_id ORDER BY first_year ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS price
+FROM sales
+ORDER BY 1;
+
+--Solution 2:
+SELECT product_id
+		,year AS first_year
+		,quantity
+		,price
+FROM(SELECT product_id
+		,year
+		,quantity
+		,price
+		,DENSE_RANK()OVER(PARTITION BY product_id ORDER BY year ASC) AS RNK
+	FROM sales)
+WHERE rnk = 1
+ORDER BY 1;
