74637f057af8624ba7c51414cca4f10a6ef5f3eb
yd188 <39896319+yd188@users.noreply.github.com>
2019-11-24 12:10:33 -0800

    Update 595. Big Countries.sql

--- Database/595. Big Countries.sql
+++ Database/595. Big Countries.sql
@@ -24,6 +24,8 @@ For example, according to the above table, we should output:
 +--------------+-------------+--------------+
 */
 
-select name, population, area 
-from world 
-where area > 3000000 or population>25000000;
+SELECT name
+      ,population
+      ,area 
+FROM world 
+WHERE area > 3000000 OR population > 25000000;
