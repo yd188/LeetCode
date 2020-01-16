8f4e4db7fdfdd3a8b0c022b1056dc867c7d257ba
yd188 <YDong@Yuans-MacBook-Pro.local>
2020-01-16 06:58:30 -0800

    add 1295

--- Algorithms/1313. Decompress Run-Length Encoded List.py
+++ Algorithms/1313. Decompress Run-Length Encoded List.py
@@ -1,14 +1,7 @@
 '''
 1313. Decompress Run-Length Encoded List
-Easy
 
-29
 
-126
-
-Add to List
-
-Share
 We are given a list nums of integers representing a list compressed with run-length encoding.
 
 Consider each adjacent pair of elements [a, b] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are a elements with value b in the decompressed list.
